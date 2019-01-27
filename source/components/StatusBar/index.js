58b0f7f0693636931b1f7e80cb5953056951c33d
TheGeorgich <vitaly.struk@eleken.co>
2019-01-27 15:06:55 +0200

    react-online-lesson-3

--- source/components/StatusBar/index.js
+++ source/components/StatusBar/index.js
@@ -1,26 +1,26 @@
 // Core
 import React, { Component } from 'react';
 
-// // Instruments
-// import Styles from './styles.m.css';
+// Instruments
+import Styles from './styles.m.css';
 
-// export default class StatusBar extends Component {
-//     render() {
-//         const {
-//             avatar,
-//             currentFirstName,
-//             currentLastName,
-//         } = this.props;
+export default class StatusBar extends Component {
+    render() {
+        const {
+            avatar,
+            currentFirstName,
+            currentLastName,
+        } = this.props;
 
-//         return (
-//             <section className = { Styles.statusBar }>
-//                 <button>
-//                     <img src = { avatar } />
-//                     <span>{ currentFirstName }</span>
-//                     &nbsp;
-//                     <span>{ currentLastName }</span>
-//                 </button>
-//             </section>
-//         );
-//     }
-// }
+        return (
+            <section className = { Styles.statusBar }>
+                <button>
+                    <img src = { avatar } />
+                    <span>{ currentFirstName }</span>
+                    &nbsp;
+                    <span>{ currentLastName }</span>
+                </button>
+            </section>
+        );
+    }
+}
